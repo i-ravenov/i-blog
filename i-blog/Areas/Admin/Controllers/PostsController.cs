df8f9a19829b6dbd3ea8769e909045ea19895eb0
Ivan Voronov <ioann.voronov@gmail.com>
2016-03-18 19:14:54 +0200

    Created new and post actions for post controller.

--- i-blog/Areas/Admin/Controllers/PostsController.cs
+++ i-blog/Areas/Admin/Controllers/PostsController.cs
@@ -1,4 +1,6 @@
-﻿using System.Data.Entity;
+﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Web.Mvc;
 using i_blog.Areas.Admin.ViewModels;
@@ -30,5 +32,68 @@ namespace i_blog.Areas.Admin.Controllers
             });
 
         }
+
+
+        public ActionResult New()
+        {
+            return View("form", new PostsForm
+            {
+                IsNew = true
+            });
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var post = db.Posts.Find(id);
+            if (post == null)
+                return HttpNotFound();
+
+            return View("form", new PostsForm()
+            {
+                IsNew = false,
+                PostId = id,
+                Content = post.Content,
+                Slug = post.Slug,
+                Title = post.Title
+            });
+        }
+
+        [HttpPost]
+        public ActionResult Form(PostsForm form)
+        {
+            form.IsNew = form.PostId == null;
+
+            if (!ModelState.IsValid)
+                return View(form);
+
+            Post post;
+            if (form.IsNew)
+            {
+                post = new Post()
+                {
+                    CreatedAt = DateTime.UtcNow,
+                    UserId = 1,                     // !!! need to be redone
+                };
+            }
+            else
+            {
+                post = db.Posts.Find(form.PostId);
+
+                if (post == null)
+                    return HttpNotFound();
+
+                post.UpdatedAt = DateTime.UtcNow;
+            }
+
+            post.Title = form.Title;
+            post.Slug = form.Slug;
+            post.Content = form.Content;
+
+            db.Posts.AddOrUpdate(post);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
     }
 }
--- i-blog/Areas/Admin/ViewModels/Posts.cs
+++ i-blog/Areas/Admin/ViewModels/Posts.cs
@@ -1,4 +1,5 @@
-﻿using i_blog.Infrastructure;
+﻿using System.ComponentModel.DataAnnotations;
+using i_blog.Infrastructure;
 using i_blog.Models;
 
 namespace i_blog.Areas.Admin.ViewModels
@@ -7,4 +8,20 @@ namespace i_blog.Areas.Admin.ViewModels
     {
         public PagedData<Post> Posts { get; set; }
     }
+
+    public class PostsForm
+    {
+        public bool IsNew { get; set; }
+        public int? PostId { get; set; }
+
+        [Required, MaxLength(128)]
+        public string Title { get; set; }
+
+        [Required, MaxLength(128)]
+        public string Slug { get; set; }
+
+        [Required, DataType(DataType.MultilineText)]
+        public string Content { get; set; }
+    }
+
 }
