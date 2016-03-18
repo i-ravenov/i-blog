188ccc6888a6e28152bc044b79e9838910184545
Ivan Voronov <ioann.voronov@gmail.com>
2016-03-18 19:01:57 +0200

    Index Post action in PostController Admin

--- i-blog/Areas/Admin/Controllers/PostsController.cs
+++ i-blog/Areas/Admin/Controllers/PostsController.cs
@@ -1,13 +1,34 @@
-﻿using System.Web.Mvc;
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+using i_blog.Areas.Admin.ViewModels;
+using i_blog.DAL;
+using i_blog.Infrastructure;
+using i_blog.Models;
 
 namespace i_blog.Areas.Admin.Controllers
 {
     [Authorize(Roles = "admin")]
     public class PostsController : Controller
     {
-        public ActionResult Index()
+        private const int PostsPerPage = 5;
+        private BlogContext db = new BlogContext();
+
+        public ActionResult Index(int page = 1)
         {
-            return View();
+            var totalPostCount = db.Posts.Count();
+
+            var currentPostPage = db.Posts
+                .OrderByDescending(c => c.CreatedAt)
+                .Skip((page - 1) * PostsPerPage)
+                .Take(PostsPerPage)
+                .ToList();
+
+            return View(new PostsIndex
+            {
+                Posts = new PagedData<Post>(currentPostPage, totalPostCount, page, PostsPerPage)
+            });
+
         }
     }
 }
---
+++ i-blog/Areas/Admin/ViewModels/Posts.cs
@@ -0,0 +1,10 @@
+﻿using i_blog.Infrastructure;
+using i_blog.Models;
+
+namespace i_blog.Areas.Admin.ViewModels
+{
+    public class PostsIndex
+    {
+        public PagedData<Post> Posts { get; set; }
+    }
+}
